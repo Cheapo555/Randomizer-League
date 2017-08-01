0af550a17f13f6a92867cd25d154da6723d2ae12
Cheapo555 <vivek.m.elashoff@gmail.com>
2017-08-01 14:42:33 -0700

    Item Set

--- LeagueRandomizer.java
+++ LeagueRandomizer.java
@@ -1,9 +1,109 @@
 import java.util.ArrayList;
-
+import java.util.Random;
 public class LeagueRandomizer {
-  public static void main(String[] args) {
+  public static void main(String[] args)
+  {
+    Random rng = new Random;
 
-    ArrayList<String> champions = new ArrayList;
-    
+    ArrayList<Champions> champions = new ArrayList;
+    ArrayList<String> lanes = new ArrayList;
+    ArrayList<Items> items = new ArrayList;
+    ArrayList<String> summoners = new ArrayList;
+    ArrayList<String> maxAbilities = new ArrayList;
+
+    int laneRNG = rng.nextInt(lane.size() - 1);
+    int championsRNG = rng.nextInt(champions.size() - 1);
+    int summonersRNG = rng.nextInt(summoners.size() - 1);
+    int itemsRNG = rng.nextInt(items.size() - 1);
+    System.out.println();
+    Items abyssalMask = new Items("Abyssal Mask"),
+    Items adaptiveHelm = new Items("Adaptive Helm"),
+    Items archangelsStaff = new Items("Archangel's Staff"),
+    Items ardentCenser = new Items("Ardent Censer"),
+    Items athenesUnholyGrail = new Items("Athene's Unholy Grail"),
+    Items bannerOfCommand = new Items("Banner of Command"),
+    Items bansheesVeil = new Items("Banshee's Veil"),
+    Items berserkersGreaves = new Items("Berserker's Greaves"),
+    Items bladeOfTheRuinedKing = new Items("Blade of the Ruined King"),
+    Items bootsOfMobility = new Items("Boots of Mobility"),
+    Items bootsOfSwiftness = new Items("Boots of Swiftness"),
+    Items deadMansPlate = new Items("Dead Man's Plate"),
+    Items deathsDance = new Items("Death's Dance"),
+    Items duskbladeOfDraktharr = new Items("Duskblade of Draktharr"),
+    Items edgeOfNight = new Items("Edge of Night"),
+    Items enchantmentBloodrazorRed = new Items("Red Enchantment: Bloodrazor"),
+    Items enchantmentBloodrazorGreen = new Items("Green Enchantment: Bloodrazor"),
+    Items enchantmentBloodrazorBlue = new Items("Blue Enchantment: Bloodrazor"),
+    Items enchantmentCinderhulkRed = new Items("Red Enchantment: Cinderhulk"),
+    Items enchantmentCinderhulkGreen = new Items("Green Enchantment: Cinderhulk"),
+    Items enchantmentCinderhulkBlue = new Items("Blue Enchantment: Cinderhulk"),
+    Items enchantmentRunicEchoesRed = new Items("Red Enchantment: Runic Echoes"),
+    Items enchantmentRunicEchoesGreen = new Items("Green Enchantment: Runic Echoes"),
+    Items enchantmentRunicEchoesBlue = new Items("Blue Enchantment: Runic Echoes"),
+    Items enchantmentWarriorRed = new Items("Red Enchantment: Warrior"),
+    Items enchantmentWarriorGreen = new Items("Green Enchantment: Warrior"),
+    Items enchantmentWarriorBlue = new Items("Blue Enchantment: Warrior"),
+    Items essenceReaver = new Items("Essence Reaver"),
+    Items eyeOfTheEquinox = new Items("Eye of the Equinox"),
+    Items eyeOfTheOasis = new Items("Eye of the Oasis"),
+    Items eyeOfTheWatchers = new Items("Eye of the Watchers"),
+    Items faceOfTheMountain = new Items("Face of the Mountain"),
+    Items frostQueensClaim = new Items("Frost Queen's Claim"),
+    Items frozenHeart = new Items("Frozen Heart"),
+    Items frozenMallet = new Items("Frozen Mallet"),
+    Items gargoyleStoneplate = new Items("Gargoyle Stoneplate"),
+    Items gaurdianAngel = new Items("Gaurdian Angel"),
+    Items guinsoosRageblade = new Items("Guinsoos Rageblade"),
+    Items hextechGLP800 = new Items("Hextech GLP-800"),
+    Items hextechGunblade = new Items("Hextech Gunblade"),
+    Items hextechProtobelt01 = new Items("Hextech Protobelt-01"),
+    Items icebornGauntlet = new Items("Iceborn Gauntlet"),
+    Items infinityEdge = new Items("Infinity Edge"),
+    Items ionianBootsOfLucidity = new Items("Ionian Boots Of Lucidity"),
+    Items knightsVow = new Items("Knight's Vow"),
+    Items liandrysTorment = new Items("Liandrys Torment"),
+    Items lichBane = new Items("Lich Bane"),
+    Items locketOfTheIronSolari = new Items("Locket Of The Iron Solari"),
+    Items lordDominiksRegards = new Items("Lord Dominik's Regards"),
+    Items ludensEcho = new Items("Luden's Echo"),
+    Items manamune = new Items("Manamune"),
+    Items mawOfTheMalmortius = new Items("Maw of the Malmortius"),
+    Items mejaisSoulstealer = new Items("Mejai's Soulstealer"),
+    Items mercurialScimitar = new Items("Mercurial Scimitar"),
+    Items mercurysTreads = new Items("Mercury's Treads"),
+    Items mikaelsCrucible = new Items("Mikael's Crucible"),
+    Items morellonomicon = new Items("Morellonomicon"),
+    Items mortalReminder = new Items("Mortal Reminder"),
+    Items nashorsTooth = new Items("Nashor's Tooth"),
+    Items ninjaTabi = new Items("Ninja Tabi"),
+    Items ohmwrecker = new Items("Ohmwrecker"),
+    Items phantomDancer = new Items("Phantom Dancer"),
+    Items rabidonsDeathcap = new Items("Rabidon's Deathcap"),
+    Items randiunsOmen = new Items("Randiun's Omen"),
+    Items rapidFirecannon = new Items("Rapid Firecannon"),
+    Items ravenousHydra = new Items("RavenousHydra"),
+    Items redemption = new Items("Redemption"),
+    Items righteousGlory = new Items("Righteous Glory"),
+    Items rodOfAges = new Items("Rod of Ages"),
+    Items runaansHurricane = new Items("Runaan's Hurricane"),
+    Items rylaisCrystalScepter = new Items("Rylai's Crystal Scepter"),
+    Items spiritVisage = new Items("Spirit Visage"),
+    Items statikkShiv = new Items("Statikk Shiv"),
+    Items steraksGage = new Items("Sterak's Gage"),
+    Items sunfireCape = new Items("Sunfire Cape"),
+    Items talismanOfAscension = new Items("Talisman of Ascension"),
+    Items theBlackCleaver = new Items("The Black Cleaver"),
+    Items theBloodthirster = new Items("The Bloodthirster"),
+    Items thornmail = new Items("Thornmail"),
+    Items titanicHydra = new Items("Titanic Hydra"),
+    Items trinityForce = new Items("Trinity Force"),
+    Items voidStaff = new Items("Void Staff"),
+    Items warmogsArmor = new Items("Warmog's Armor"),
+    Items witsEnd = new Items("Wit's End"),
+    Items yoummuusGhostblade = new Items("Yoummuu's Ghostblade"),
+    Items zekesConvergance = new Items("Zeke's Convergance"),
+    Items zhonyasHourglass = new Items("Zhonya's Hourglass"),
+    Items zzrotPortal = new Items("Zz'rot Portal"),
+
   }
 }
